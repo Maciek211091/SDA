9eac0f1a9c269b5f8085ef169f812db1fa1f7fe1
Maciej_Lesiak <maciej.lesiak91@gmail.com>
2020-08-22 11:17:18 +0200

    Reading files with decorator v2.0

--- READ_FROM_FILES.py
+++ READ_FROM_FILES.py
@@ -1,34 +1,30 @@
 def read_from_files(file_paths: list) -> str:
-
     new_str = ''
 
     for i in file_paths:
         with open(i, 'r') as f:
             a = f.read()
-            new_str += a
+            new_str += a + '\n'
 
     return new_str
 
 
 file_paths_to_read = ['/home/maciej/PycharmProjects/Wzroce-projektowe/text1.txt',
-              '/home/maciej/PycharmProjects/Wzroce-projektowe/text2.txt']
+                      '/home/maciej/PycharmProjects/Wzroce-projektowe/text2.txt']
 
 
-#print(read_from_files(file_paths_to_read))
+# print(read_from_files(file_paths_to_read))
 
 
-def read_from_files_decor(func, file_paths: list):
+def read_from_files_decor(func, additional_message, file_paths: list):
     def wrapper():
-        print('---')
-        print('Reading from files\n')
-        print(func(file_paths))
-        print('\nReading ended')
-        print('---')
+        result = (additional_message + '\nReading from files\n----\n'
+                  + func(file_paths) + '----\nReading ended')
+        return result
     return wrapper
 
 
-wrapped_func = read_from_files_decor(read_from_files, file_paths_to_read)
-
+wrapped_func = read_from_files_decor(read_from_files, 'Maciej czyta', file_paths_to_read)
 
 if __name__ == '__main__':
-    wrapped_func()
+    print(wrapped_func())
